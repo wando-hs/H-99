ee410f13497a3b752cd75cf747aa9efb50a84fd6
Fernando Oliveira <fernando@fholiveira.com>
2016-08-03 23:07:00 -0300

    Dojo

---
+++ src/Problem61.hs
@@ -0,0 +1,28 @@
+module Problem61 (Tree(..), countLeaves, leaves, leaf) where
+
+data Tree a = Empty | Node a (Tree a) (Tree a)
+    deriving (Show)
+
+countLeaves :: Tree a -> Integer
+countLeaves Empty = 0
+countLeaves (Node _ Empty Empty) = 1
+countLeaves (Node _ left right) = countLeaves left + countLeaves right
+
+leaves :: Tree a -> [a]
+leaves Empty = []
+leaves (Node a Empty Empty) = [a]
+leaves (Node _ left right) = leaves left ++ leaves right
+
+leaf :: Show a => a -> Tree a
+leaf x = Node x Empty Empty
+
+internals :: Tree a -> [a]
+internals Empty = []
+internals (Node _ Empty Empty) = []
+internals (Node x left right) = [x] ++ internals left ++ internals right
+
+atLevel :: Tree a -> Integer -> [a]
+atLevel (Node x _ _) 1 = [x]
+atLevel (Node _ left right) n = recur left ++ recur right
+    where recur tree = atLevel tree $ pred n
+
